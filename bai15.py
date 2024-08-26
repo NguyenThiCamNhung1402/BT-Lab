6132bc706c3f6ee55c2b2532cc3e973bd24e88a6
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-08-26 16:02:44 +0700

    bai15.py

--- bai15.py
+++ bai15.py
@@ -7,12 +7,12 @@ Created on Sun Aug 25 23:43:28 2024
 import math
 a = float(input("Nhập giá trị của a: "))
 b = float(input("Nhập giá trị của b: "))
-if b == 0:
-    print("Lỗi: b không thể bằng 0.")
+if a == 0 or b == 0: 
+    print("Lỗi: a,b không thể bằng 0.")
 else:
-    sqrt_a, sqrt_b = math.sqrt(a), math.sqrt(b)
-    fourth_root_a, fourth_root_b = a ** 0.25, b ** 0.25
-    p1 = (sqrt_a - sqrt_b) / (fourth_root_a - fourth_root_b) if fourth_root_a != fourth_root_b else float('inf')
-    p2 = (sqrt_a + (a * b) ** 0.25) / (fourth_root_a + fourth_root_b)
-    print("Kết quả biểu thức:",p1 - p2)
-    print("Căn bậc 4 của b:",fourth_root_b)
+    can_a, can_b = math.sqrt(a), math.sqrt(b)
+    can3_a, can3_b, can3_ab = a ** (1/3), b ** (1/3), (a*b)**(1/3)
+    p1 = ((a+b) / (can3_a + can3_b) ) - (can3_ab)
+    p2 = (can3_a - can3_b) ** 2
+    print("Kết quả biểu thức:",p1 / p2)
+
