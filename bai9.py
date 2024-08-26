a151b790f4d1f14648e8629ef9bc40fd2162bca1
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-08-26 18:06:20 +0700

    bai9.py

--- bai9.py
+++ bai9.py
@@ -5,11 +5,17 @@ Created on Sun Aug 25 23:40:26 2024
 @author: Nguyễn Thị Cẩm Nhung - 23712471
 """
 print("======= MENU =========")
-print(" 1. Hu tieu")
-print(" 2. Chao long")
-print(" 3. Banh canh")
-print(" 4. Bun rieu")
-print(" 5. Pho bo")
+print(" 1. Hu tieu"," \n 2. Chao long","\n 3. Banh canh","\n 4. Bun rieu","\n 5. Pho bo")
 print("======================")
-print("Moi nhap lua chon:")
+i = int(input("Moi nhap lua chon:"))
 print("======================")
+if i== 1 :
+    print("Hu tieu")
+elif i== 2 :
+    print("Chao long")
+elif i== 3:
+    print("Banh canh")
+elif i== 4:
+    print("Bun rieu")
+else :
+    print("Pho bo")
